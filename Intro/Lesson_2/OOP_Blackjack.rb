094d5af01750359c31cc51e64b0e382b2efc8416
jamelablack <jamela.black@gmail.com>
2014-10-13 11:09:27 -0400

    update to OO_Blackjack

--- Intro/Lesson_2/OOP_Blackjack.rb
+++ Intro/Lesson_2/OOP_Blackjack.rb
@@ -1,8 +1,32 @@
 #Tips for getting started with the Blackjack game:
-#1. Write out your requorements in paragraph form
+#1. Write out your requirrements in paragraph form
 #2. Extract major nouns -> classes
 #3. Extract major verbs  -> instance methods
 #4. Group instance methods into classes
+=begin
+=begin
+General:
+- Goal: The sum value of your cards must equal 21 (blackjack)
+- Both Player and Dealer (Computer) are dealt two cards to start the game.
+- All face cards are worth whatever numerical value they show.
+- Suits are worth 10. Aces are worth 1 or 11 (whichever is more favorable)
+
+How to Play:
+- After being dealt the initial 2 cards, the player goes first and can choose to either "hit" or "stay"
+- Hitting means dealing another card. 
+- If the player's card sum up to be greater than 21, the Player has "busted" and lost.
+- If the sum is 21, the Player wins. 
+- If the sum is less than 21, the player can choose to "hit" or "stay" again.
+- If the player "hits", then repeat the above, but if the players stays, then the player's total value is saved,
+  and turned over to the dealer.
+
+In Order to Win:
+- The Dealer must hit until she has at least 17.
+- If the dealer busts, then the player wins.
+- If the dealer hits 21, the dealer wins. 
+- If however, the dealer stays, then we compare the sumes of the two hands between the player and dealer;
+  higher value wins.=end
+=end
 
 # Possible nouns
 	# Player
