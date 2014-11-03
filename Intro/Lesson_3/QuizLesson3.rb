26b33f12ec0950907d3a183f9fb5bf44e1e4f59d
jamelablack <jamela.black@gmail.com>
2014-11-02 21:11:18 -0500

    minor updates

---
+++ Intro/Lesson_3/QuizLesson3.rb
@@ -0,0 +1,34 @@
+# QuizLesson3.rb
+
+# Quiz: Lesson 3
+
+# Please review this quiz, which covers lesson 3 materials.
+
+# What is HTML? What is CSS? What is Javascript?
+
+# What are the major parts of an HTTP request?
+
+# What are the major parts of an HTTP response?
+
+# How do you submit an HTTP POST request, with a "username" attribute set to "bob"? What if we wanted a GET request instead?
+
+# Why is it important for us, as web developers, to understand that HTTP is a "stateless" protocol?
+
+# If the internet is just HTTP requests/responses, why do we only use browsers to interface with web applications? Are there any other options?
+
+# What is MVC, and why is it important?
+
+# The below questions are about Sinatra:
+
+# At a high level, how are requests processed?
+
+# In the controller/action, what's the difference between rendering and redirecting?
+
+# In the ERB view template, how do you show dynamic content?
+
+# Given what you know about ERB templates, when do you suppose the ERB template is turned into HTML?
+
+# What's the role of instance variables in Sinatra?
+
+# Solutions are in the first lecture video of lesson 4.
+
