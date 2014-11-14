741beb768da5c2d1fa539c79f71dd91e9bfb1102
jamelablack <jamela.black@gmail.com>
2014-11-14 18:51:47 -0500

    What if I don't feel like writing a message.

--- Intro/Lesson_4/LectureNotes1.rb
+++ Intro/Lesson_4/LectureNotes1.rb
@@ -1,7 +1,7 @@
 Ajax is used in a scernario when elements on the page are changing but you only want thos elements
 to change rather reload (reconstituting the state of your application) each time. 
 Pages can be computationally expensive and create a bad user interface. 
-Ajax will issue the request behind the sences and the when the response comes back, your Javascript code must handle that data. 
+Ajax will issue the request behind the scences and the when the response comes back, your Javascript code must handle that data. 
 This is using unobstrusive Javascript. For example, if you have likes on your Facebook page, we will want 
 to see an increment in the number of likes and the like button go from "like" to "unlike".
 However, for our use case, only the URL won't change.
@@ -15,7 +15,7 @@ You must add the <script src="/application.js"></script> to the layout.erb
 Google hosts Jquery for you!
 1.x snippet: <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-- # is an achor tage
+- # is an anchor tag
 JQuery
 .class - same style
  #id - unique element on your DOM
@@ -36,11 +36,81 @@ So more like dashes and sprinkles of Jquery rather than overload of JQuery
 
 =================Part 2
 
-Adding JQuery to the "hit-button"
+Hijacking (Adding JQuery to) the "hit-button"
 
 $(document).ready(function() {
 	$('#hit_form input').click(function() {
 		alert('hit button clicked!');
 		return false;
 		});
-	})
+	});
+
+use console.log ( to inspect/debug in Firefox)
+
+
+Using Ajax
+$(document).ready(function() {
+	#$('#hit_form input').click(function() { #must be rebind with the input again after indcating layout false
+		$document).on(click, '#hit_form input', function()) {
+
+		$.ajax({ #this is a hash, much include comma after entry
+			type: 'POST',
+			url: '/game/player/hit'
+			#data: #also known as parameters
+			)}.done(function(msg) {
+				#$('#game').html(msg); #create div for game. Can't use HTML b/c its creating a duplicate game div id
+				$('#game').replaceWith(msg);
+				});
+			});
+
+		return false;
+		});
+	});
+
+
+
+#main.rb
+post '/game/play/hit'
+.......
+.........
+...........
+erb: game, layout false
+
+
+#game.erb
+Need to replace the entire template because we have dynamic content all throughout
+Surround div id called game
+<div id="game">
+.........
+......
+.....
+</div>
+
+
+Deployment Options
+1. Self-managed
+- Shared host (no longer popular)
+- VPS (most popular)
+	- AWS, Rackspace, Linode, Digital Ocean
+	- must have sys admin knowledge
+- Dedicated Server (hardware) - most expensive
+
+2. Managed - no sys admin needs, no access to file system
+- Heroku
+- Google App Engine
+- Ninefold
+
+
+
+
+Good Reads:
+Eloquent Ruby
+Rails Tutorial
+Agile Programming with Rails
+
+
+
+
+
+
+
