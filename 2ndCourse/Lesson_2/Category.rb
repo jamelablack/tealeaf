d2d7332a73f37c73385798e9f3a584de43d520de
jamelablack <jamela.black@gmail.com>
2014-12-29 12:56:36 -0500

    Notes from Lecture 4

---
+++ 2ndCourse/Lesson_2/Category.rb
@@ -0,0 +1,15 @@
+1. Create link to new category in nav bar, click link -> invalid route
+2. Create resource under routes, specify only those needed
+3. Add New action in controller
+4. Create new.html.erb form, create form
+.
+.
+.
+8. Add drop down navigation link
+9. Display associated category posts
+Refactor into partials, passing variable, learn convention
+File naming convention -> Learn it
+Memorize link_to convention
+Know category.post(category)
+9. Displaying "All Posts >> Category" on show Category page( show.html.erb), using html_safe
+10. Refactor for 'shared/errors' partial in new.html.erb - category and _form.html.erb - posts.
